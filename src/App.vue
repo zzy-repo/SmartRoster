2491a5b0aca5a09d398f3307dac90cfc071be2ee
zzy <1061342204@qq.com>
2025-01-20 10:57:06 +0800

    fix:'Gitee's anti-hotlinking mechanism prevents external access to images by checking the Referer header in requests.'

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,15 @@ import {Plan, Github} from '@icon-park/vue-next'
         </a>
       </div>
       <div class="left flex">
-        <a class="no-underline transition-all duration-250 hover:scale-110"
+        <a class="transition-all duration-250 hover:scale-110"
            href="https://github.com/zzy-repo/SmartRoster"
            target="_blank">
           <Github theme="outline" size="27" fill="#333"/>
         </a>
         <div class="userInfo ml-2 flex flex-items-center">
-          <img class="rounded-lg" src="https://gitee.com/zzy2401/picbed/raw/master/images/小熊猫-白底.jpg" alt="头像">
+          <img class="rounded-full w-[27px] h-[27px] b-1 b-solid b-gray-300 transition-all duration-250 hover:scale-110"
+               src="https://gitee.com/zzy2401/picbed/raw/master/images/小熊猫-白底.jpg"
+               alt="头像">
           <p>zzy</p>
         </div>
       </div>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import './style.css'
 import 'virtual:uno.css'
 import 'animate.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
