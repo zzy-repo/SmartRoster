154b9dc893d26b97e63f35c795efbd173a85b6fd
zzy <1061342204@qq.com>
2025-02-17 08:42:20 +0800

    updata:change the style of sidebar

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -18,21 +18,22 @@ const { menuData, isCollapsed } = props
 
 <template>
   <ul class="flex flex-col items-center">
+    <li class="w-90% h-8">菜单</li>
     <li
       v-for="(item, index) in menuData" :key="index"
-      class="rounded-lg w-90% h-10 flex items-center justify-center hover:bg-#f1f5fa group transition-all transition-duration-300 active:scale-80"
+      class="rounded-md w-90% h-8 flex items-center justify-center hover:bg-#f1f5fa group transition-all transition-duration-300 active:scale-80"
     >
       <a
-        class="w-full h-full no-underline   flex items-center justify-center"
+        class="w-full h-full no-underline flex items-center justify-start"
         :href="item.link"
       >
         <component
-          :is="item.icon" class="flex items-center" theme="outline" size="30" fill="#333"
+          :is="item.icon" class="flex items-center m-r-[3px]" theme="outline" size="20" fill="#333"
           stroke-linecap="square"
         />
         <p
           v-if="!isCollapsed"
-          class="font-300 w-60% flex justify-center color-#333 group-hover:color-#0065ff"
+          class="text-sm flex justify-center color-#333 group-hover:color-#0065ff"
         >
           {{ item.name }}
         </p>
