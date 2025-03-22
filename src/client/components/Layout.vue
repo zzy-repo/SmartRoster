89fd09d35a3f5304a430781b2a75948e2505a474
zzy <1061342204@qq.com>
2025-03-22 22:49:39 +0800

    beautify

--- src/client/components/Layout.vue
+++ src/client/components/Layout.vue
@@ -26,16 +26,17 @@ const menuItems = [
 
 <template>
   <div class="common-layout">
-    <el-container>
-      <el-header>
-        <router-link to="/login">登录</router-link>
+    <el-container style="height: 100vh;">
+      <el-header style="background-color: #409EFF; color: white; display: flex; align-items: center; justify-content: space-between; padding: 0 20px;">
+        <h1 style="font-size: 24px; margin: 0;">SmartRoster</h1>
+        <router-link to="/login" style="color: white; text-decoration: none;">登录</router-link>
       </el-header>
       <el-container>
-        <el-aside width="200px">
+        <el-aside width="200px" style="background-color: #f4f4f4; box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);">
           <!-- 使用 SideMenu 组件 -->
           <SideMenu :menuItems="menuItems" :defaultActive="activeMenu" />
         </el-aside>
-        <el-main>
+        <el-main style="padding: 20px; background-color: #fafafa;">
           <router-view />
         </el-main>
       </el-container>
