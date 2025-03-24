a526848ae67e3da6f4e25ac3c3b3e9e5817a3180
zzy <1061342204@qq.com>
2025-03-24 23:40:37 +0800

    chore: 删除roster模块及其相关文件
    清理项目中的roster模块，包括其子模块和所有相关文件，以简化代码库结构

--- roster/models/shift.py
+++
@@ -1,6 +0,0 @@
-class Shift:
-    def __init__(self, day, start_time, end_time, required_positions):
-        self.day = day
-        self.start_time = start_time
-        self.end_time = end_time
-        self.required_positions = required_positions
--- roster/utils/time_utils.py
+++
@@ -1,8 +0,0 @@
-def time_to_minutes(t):
-    hours, mins = map(int, t.split(":"))
-    return hours * 60 + mins
-
-def calculate_shift_duration(shift):
-    start = time_to_minutes(shift.start_time)
-    end = time_to_minutes(shift.end_time)
-    return (end - start) / 60
